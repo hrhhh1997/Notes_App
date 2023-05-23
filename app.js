73dbf6f0f75d6574898d250f7d2623da738ca4ec
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-23 16:19:11 +0000

    Perform update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-const listItems = [];
+const listItems = [{title: "Lorem ipsum dolor sit amet", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "}, {title: "Duis aute irure dolor in", body: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}];
 
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render('todo', {});
+    res.render('todo', {listItems: listItems});
 });
 
 app.listen(process.env.PORT || 3000, () => {
